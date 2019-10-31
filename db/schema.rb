46ff1efe07be5aeeed7e8ee89c1ec06707fde574
tokuchi0823 <tokuchi.ryu.th@gmail.com>
2019-10-31 14:26:07 +0000

    save user-edit

--- db/schema.rb
+++ db/schema.rb
@@ -40,12 +40,12 @@ ActiveRecord::Schema.define(version: 20191027151325) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2019-10-27 23:00:00"
-    t.datetime "work_time", default: "2019-10-27 22:30:00"
+    t.datetime "basic_time", default: "2019-10-30 23:00:00"
+    t.datetime "work_time", default: "2019-10-30 22:30:00"
     t.string "employee_number"
-    t.integer "uid"
-    t.datetime "designated_work_start_time", default: "2019-10-28 00:00:00"
-    t.datetime "designated_work_end_time", default: "2019-10-28 09:00:00"
+    t.string "uid"
+    t.datetime "designated_work_start_time", default: "2019-10-31 00:00:00"
+    t.datetime "designated_work_end_time", default: "2019-10-31 09:00:00"
     t.boolean "superior", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
